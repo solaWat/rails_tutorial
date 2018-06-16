40823795d230706e45823d3f2beeaec7549e2399
solaWat_rails <s.spthx.555@gmail.com>
2018-06-16 18:30:05 +0900

    finish static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def hello
-    render html: "hello"
-  end
+  # def hello
+  #   render html: "hello"
+  # end
+  
 end
