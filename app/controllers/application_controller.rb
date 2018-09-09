6166dc62390eb91d7f4bd3b9c2d6d6ef6bb73959
solaWat_rails <s.spthx.555@gmail.com>
2018-09-09 16:23:59 +0900

    Implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  # def hello
-  #   render html: "hello"
-  # end
+  include SessionsHelper
   
   
 end
